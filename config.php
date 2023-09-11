63fd2805b51e5acbfb50bce31f84610f9b85665e
9dgamer <56701817+9d-gamer@users.noreply.github.com>
2023-09-11 08:00:24 +0500

    update

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-    $config['URL'] = 'http://localhost';
+    $config['URL'] = 'http://localhost'; // don't use (/) in the end also don't forget to mention the folder where you added this project in your hosting for example for covid folder inside hosting it should pe
+                                         // http://localhost/covid
     $config['DB_URL'] = 'localhost:3306';
     $config['DB_USERNAME'] = 'root';
     $config['DB_PASSWORD'] = '';
