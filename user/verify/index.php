8a4092fe90f4e66a3c037e038437da51316271f0
Pixel Nomad <56701817+Pixel-Nomad@users.noreply.github.com>
2026-01-19 04:27:59 -0800

    Clear SMTP password in index.php
    Removed the password for the SMTP configuration.

--- user/verify/index.php
+++ user/verify/index.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['isloggedin'])) {
                         $mail->Host       = 'smtp.gmail.com';
                         $mail->SMTPAuth   = true;
                         $mail->Username   = 'emergency.med.svc@gmail.com';
-                        $mail->Password   = 'lqlgqkdcyxeqijif';
+                        $mail->Password   = '';
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                         $mail->Port       = 587;
                         // Recipients
